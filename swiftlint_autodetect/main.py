484fae59bc477fdfd7f21cd37bb5b48854ef3391
Jonathan Wight <jwight@mac.com>
2021-12-23 12:18:06 -0800

    Initial source code.

---
+++ swiftlint_autodetect/main.py
@@ -0,0 +1,79 @@
+import subprocess
+from pathlib import Path
+from shutil import which
+import shlex
+from typing import Dict
+import yaml
+import re
+import sys
+import typer
+
+app = typer.Typer()
+
+@app.callback()
+def swiftlint_autodetect(path: str):
+    swiftlint = which("swiftlint")
+
+    output = (
+        subprocess.check_output(shlex.split(f"{swiftlint} rules"), cwd="/tmp")
+        .decode("utf8")
+        .splitlines()
+    )
+
+    keys = [field.strip() for field in output[1].split("|")][1:-1]
+    rows = output[3:-1]
+
+    rules = []
+
+    for row in rows:
+        values = [field.strip() for field in row.split("|")][1:-1]
+        rule = dict(zip(keys, values))
+        rules.append(rule)
+        # if rule["analyzer"] == "yes":
+        #     analyzer_rules.append(rule)
+        # else:
+        #     lint_rules.append(rule)
+
+    # {'identifier': 'anonymous_argument_in_multiline_closure', 'opt-in': 'yes', 'correctable': 'no', 'enabled in your config': 'no', 'kind': 'idiomatic', 'analyzer': 'no', 'configuration': 'warning'}
+
+    config = {
+        "only_rules": [
+            rule["identifier"] for rule in rules if rule["analyzer"] == "no"
+        ],
+        "analyzer_rules": [
+            rule["identifier"] for rule in rules if rule["analyzer"] == "yes"
+        ],
+    }
+
+    yaml.safe_dump(config, open("/tmp/swiftlint.yml", "w"))
+
+    pattern = re.compile(r"^.+:\d+:\d+: (?:warning|error): .+ \((?P<rule>.+)\)$")
+    failing_rules = set()
+
+    styles = ["lint"]
+    # styles = ["lint", "analyze"]
+
+    path = Path(path).expanduser()
+
+    for style in styles:
+        args = shlex.split(
+            f"{swiftlint} {style} --config /tmp/swiftlint.yml --quiet {path}"
+        )
+        output = subprocess.run(args, capture_output=True, text=True, cwd="/tmp")
+        if output.stderr:
+            print(output.stderr)
+            exit(-1)
+
+        lines = output.stdout.splitlines()
+        for line in lines:
+            match = pattern.match(line.strip())
+            rule = match.groupdict()["rule"]
+            failing_rules.add(rule)
+
+    for line in Path("/tmp/swiftlint.yml").open().readlines():
+        line = line[:-1]
+        for rule in failing_rules:
+            if f"- {rule}" in line:
+                line = line.replace(f"- {rule}", f"# - {rule}")
+                break
+        print(line)
